ae8f1b2be15ef1118cd6f7a867d6c45ab0acf555
Catsudemo <cattmott@gmail.com>
2020-01-03 17:26:58 +0100

    Updates

---
+++ precache-manifest.398b33f582655c72607f22c93ed86edf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126317035b70ce0061ed63d0ffacc3ea",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c65e126b7e1ca5bdce80",
+    "url": "/react-portfolio/static/css/main.1517a2f1.chunk.css"
+  },
+  {
+    "revision": "7e1d51ff91709af2739c",
+    "url": "/react-portfolio/static/js/2.69653bcc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-portfolio/static/js/2.69653bcc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c65e126b7e1ca5bdce80",
+    "url": "/react-portfolio/static/js/main.7e4fa0d9.chunk.js"
+  },
+  {
+    "revision": "987b26cc3f67fd910a00",
+    "url": "/react-portfolio/static/js/runtime-main.acdb9bba.js"
+  },
+  {
+    "revision": "9cf2a70193983f9b0c86eea95714ef74",
+    "url": "/react-portfolio/static/media/profile-pic.9cf2a701.jpg"
+  }
+]);
